d592d75d4c51c4d896f628ce631f3da4b7c06364
Kryll <Jasonmanninwork@hotmail.com>
2021-06-22 01:19:02 +0800

    Began editing LoseScreen

--- src/Components/LoseScreen.js
+++ src/Components/LoseScreen.js
@@ -4,7 +4,7 @@ import './LoseScreen.css';
 export default function LoseScreen({ score, highScore, onStartClick }) {
     return (
         <div className="LoseScreen">
-            <h3>Let's see how well you know Pokémon stats...</h3>
+            {<h3>Unfortunate</h3>}
             <h3>Click Start to begin...</h3>
             <button className="StartButton" onClick={onStartClick}>Start</button>
             <h4>Current High Score: {highScore}</h4>
