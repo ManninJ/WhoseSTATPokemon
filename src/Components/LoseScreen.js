4d9ef6d9a1bf9c66ea768d99a89d977cb617930b
Kryll <Jasonmanninwork@hotmail.com>
2021-06-22 16:30:18 +0800

    Fixed LoseScreen

--- src/Components/LoseScreen.js
+++ src/Components/LoseScreen.js
@@ -6,10 +6,13 @@ export default function LoseScreen({ prevScore, highScore, onStartClick, onHomeC
         <div className="LoseScreen">
             <h3>Your score is:</h3>
             <h2>{prevScore}</h2>
-            <h3>Click Start to begin or Home to return Home...</h3>
-            <div className="Buttons">
-                <button className="StartButton" onClick={onStartClick}>Start</button>
-                <button className="HomeButton" onClick={onHomeClick}>Home</button>
+            <h3>{prevScore > highScore ? "Congratulations! \n You beat your High Score!" : "Unfortunate, better luck next time."}</h3>
+            <div className="Menu">
+                <h3>Click Start to begin or Home to return Home...</h3>
+                <div className="Buttons">
+                    <button className="StartButton" onClick={onStartClick}>Start</button>
+                    <button className="HomeButton" onClick={onHomeClick}>Home</button>
+                </div>
             </div>
             <h4>Current High Score: {highScore}</h4>
         </div>
